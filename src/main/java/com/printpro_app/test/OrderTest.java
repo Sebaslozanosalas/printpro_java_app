0170b3e45c187f32c85a474fb6c8182218082721
Sebastian <sebaslozanosalas@gmail.com>
2024-04-07 19:46:13 -0600

    Cambie la prueba a OrderTest

---
+++ src/main/java/com/printpro_app/test/OrderTest.java
@@ -0,0 +1,14 @@
+package com.printpro_app.test;
+
+import com.printpro_app.model.Order;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+
+public class OrderTest {
+
+    @Test
+    public void createOrderInstanceTest() {
+        Order order = new Order(1, "Descripción", 10);
+        assertNotNull("El objeto order no debe ser nulo", order);
+    }
+}
